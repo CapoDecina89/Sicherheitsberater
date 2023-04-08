70302d988e6b7d4c0fe1bb485a45b5d396d17073
capodecina89 <benjamin.grunow@iubh-fernstudium.de>
2023-04-08 15:08:05 +0200

    Initial Commit

---
+++ Sicherheitsberater/SicherheitsberaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SicherheitsberaterApp.swift
+//  Sicherheitsberater
+//
+//  Created by Benjamin Grunow on 08.04.23.
+//
+
+import SwiftUI
+
+@main
+struct SicherheitsberaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
